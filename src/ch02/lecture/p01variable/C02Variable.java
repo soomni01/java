5cb15b4d55f6152b8f5f841fcd8e2c5b732f528e
soomni01 <dsgh8933@naver.com>
2024-08-14 14:58:06 +0900

    변수명 작성 관습

---
+++ src/ch02/lecture/p01variable/C02Variable.java
@@ -0,0 +1,21 @@
+package ch02.lecture.p01variable;
+
+public class C02Variable {
+    public static void main(String[] args) {
+        // 변수명 작성 관습
+        // 소문자
+        int age;
+        int AGE; // x
+        int aGE; // x
+        int AGe; // x
+
+        // 여러 단어 조합 시 각 단어 앞만 대문자 (처음은 무조건 소문자)
+        int numberOfStudents;
+        int number_of_students; // snake_case ( python 관습 )
+
+        // lowerCamelCase ( 자바 변수명 관습 )
+        // UpperCamelCase ( 자바 클래스명 관습 )
+
+        //kebab-case ( 기타 등등 )
+    }
+}
