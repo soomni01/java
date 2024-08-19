72ea46559fb6b36c9596be57971d24b2528857ba
soomni01 <dsgh8933@naver.com>
2024-08-19 16:13:04 +0900

    변수 사용 범위 예제

--- src/ch02/sec11/VariableScopeExample.java
+++ src/ch02/sec11/VariableScopeExample.java
@@ -6,6 +6,6 @@ public class VariableScopeExample {
         if (v1 > 10) {
             int v2 = v1 - 10;
         }
-        int v3 = v1 + v2 + 5; //  v2 변수를 사용할 수 없기 때문에 컴파일 에러 발생
+//        int v3 = v1 + v2 +5; //  v2 변수를 사용할 수 없기 때문에 컴파일 에러 발생
     }
 }
