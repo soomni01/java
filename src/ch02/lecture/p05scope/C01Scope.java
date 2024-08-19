ab908ead0a3ec5ab06b8dcdc38e29d63d14dbbbe
soomni01 <dsgh8933@naver.com>
2024-08-19 15:36:38 +0900

    변수 사용 범위

--- src/ch02/lecture/p05scope/C01Scope.java
+++ src/ch02/lecture/p05scope/C01Scope.java
@@ -1,5 +1,30 @@
 package ch02.lecture.p05scope;
 
 public class C01Scope {
-    main
+    public static void main(String[] args) {
+        // {} : 코드 블럭
+        // 선언된 변수는 코드 블럭 내에서만 사용 가능
+
+        int a = 3;
+        System.out.println("a = " + a);
+
+        if (true) {
+            int b = 5;
+            System.out.println("b = " + b);
+            System.out.println("a = " + a);
+        }
+//        System.out.println("b = " + b); // x
+
+        int c;
+        c = 10;
+        System.out.println("c = " + c);
+        if (true) {
+            System.out.println("c = " + c);
+        }
+        System.out.println("c = " + c);
+    }
+
+    public static void method() {
+//        System.out.println("a = " + a);
+    }
 }
