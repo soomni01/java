377ac2973ad031accb46e0304106f976c1b5f6e7
soomni01 <dsgh8933@naver.com>
2024-08-19 15:35:10 +0900

    확인 문제 10번

---
+++ src/ch02/exercise/Solution10.java
@@ -0,0 +1,11 @@
+package ch02.exercise;
+
+public class Solution10 {
+    public static void main(String[] args) {
+        String str = "5";
+        byte var1 = Byte.parseByte(str);
+        int var2 = Integer.parseInt(str);
+        float var3 = Float.parseFloat(str);
+        double var4 = Double.parseDouble(str);
+    }
+}
