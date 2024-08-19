fdf638a9f2b461d909b24b2364ce25c0607b902a
soomni01 <dsgh8933@naver.com>
2024-08-19 14:53:26 +0900

    실수 타입

--- src/ch02/sec02/LongExample.java
+++ src/ch02/sec02/LongExample.java
@@ -2,6 +2,7 @@ package ch02.sec02;
 
 public class LongExample {
     public static void main(String[] args) {
+        //실수 타입
         long var1 = 10;
         long var2 = 20L;
 //        long var3 = 1000000000000;
