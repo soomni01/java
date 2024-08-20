66ab4f135ec3b8e43c7187f83f50dee688edebfe
soomni01 <dsgh8933@naver.com>
2024-08-20 16:22:05 +0900

    확인문제 5번

---
+++ src/ch03/exercise/Solution5.java
@@ -0,0 +1,19 @@
+package ch03.exercise;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Solution5 {
+    public static void main(String[] args) {
+        int lengthTop = 5;
+        int lengthBottom = 10;
+        int height = 7;
+        double area1 = ((lengthTop + lengthBottom) * height / 2.0);
+        double area2 = ((lengthTop + lengthBottom) * height * 1.0 / 2);
+        double area3 = ((double) (lengthTop + lengthBottom) * height / 2);
+        double area4 = ((double) ((lengthTop + lengthBottom) * height / 2));
+        System.out.println(area1);
+        System.out.println(area2);
+        System.out.println(area3);
+        System.out.println(area4);
+    }
+}
