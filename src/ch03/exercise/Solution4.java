815e1fb31d32bda8b557788175702bc0b066620a
soomni01 <dsgh8933@naver.com>
2024-08-20 16:21:59 +0900

    확인문제 4번

---
+++ src/ch03/exercise/Solution4.java
@@ -0,0 +1,8 @@
+package ch03.exercise;
+
+public class Solution4 {
+    public static void main(String[] args) {
+        int value = 356;
+        System.out.println(value - (value % 100));
+    }
+}
