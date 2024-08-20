3f9555c113ed449cc9a0e05c80abcd13160e8f5c
soomni01 <dsgh8933@naver.com>
2024-08-20 14:49:48 +0900

    대입연산자

---
+++ src/ch03/lecture/p06assignment/C01Assignment.java
@@ -0,0 +1,10 @@
+package ch03.lecture.p06assignment;
+
+public class C01Assignment {
+    public static void main(String[] args) {
+        // 대입(할당) 연산자 (assignment)
+        // 오른쪽 값을 왼쪽 변수에 대입(할당)
+        int a = 3;
+        int b = 5 / 2;
+    }
+}
