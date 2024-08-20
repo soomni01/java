7a18b3781c3f658242b5eae5ea8a5e1941eeb00e
soomni01 <dsgh8933@naver.com>
2024-08-20 14:50:06 +0900

    연산자 우선순위

---
+++ src/ch03/lecture/p05priority/C01Priority.java
@@ -0,0 +1,15 @@
+package ch03.lecture.p05priority;
+
+public class C01Priority {
+    public static void main(String[] args) {
+        // 연산자 우선순위
+        //책 107쪽
+
+        int a = 3 + 5 * 2;
+        System.out.println("a = " + a);
+
+        // ( ) 로 먼저 연산되는 부분을 감싸기
+        int a = 3 + 5 * 2;
+        System.out.println("a = " + a);
+    }
+}
