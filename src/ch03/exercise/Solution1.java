a23fe073babd9255980d11e80402d2aea1979595
soomni01 <dsgh8933@naver.com>
2024-08-20 11:50:17 +0900

    확인문제 1번

--- src/ch03/exercise/Solution1.java
+++ src/ch03/exercise/Solution1.java
@@ -4,7 +4,11 @@ public class Solution1 {
     public static void main(String[] args) {
         int x = 10;
         int y = 20;
-        int z = (++x) + (y--);
+//        int z = (++x) + (y--);
+        ++x;
+        int z = x + y;
+        y--;
+
         System.out.println(z);
     }
 }
