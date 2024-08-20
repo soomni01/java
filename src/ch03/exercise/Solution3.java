e4b8f8c58b25e34a967e3b72ee679e172c9c0ed6
soomni01 <dsgh8933@naver.com>
2024-08-20 10:12:55 +0900

    확인문제 3번

---
+++ src/ch03/exercise/Solution3.java
@@ -0,0 +1,13 @@
+package ch03.exercise;
+
+public class Solution3 {
+    public static void main(String[] args) {
+        int pencils = 534;
+        int students = 30;
+
+        int pencilsPerStudent = pencils / students;
+        System.out.println(pencilsPerStudent);
+        int pencilsLeft = pencils % students;
+        System.out.println(pencilsLeft);
+    }
+}
