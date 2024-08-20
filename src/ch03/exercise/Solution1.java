0508e4ca40132f07a8c0f56410e977ea409852fb
soomni01 <dsgh8933@naver.com>
2024-08-20 11:17:13 +0900

    확인문제 1번

---
+++ src/ch03/exercise/Solution1.java
@@ -0,0 +1,10 @@
+package ch03.exercise;
+
+public class Solution1 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        int z = (++x) + (y--);
+        System.out.println(z);
+    }
+}
