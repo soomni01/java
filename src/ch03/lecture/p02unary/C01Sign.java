4ba1e1187d083c816624605b9e22f6d828e048e5
soomni01 <dsgh8933@naver.com>
2024-08-20 10:58:44 +0900

    부호 연산자

---
+++ src/ch03/lecture/p02unary/C01Sign.java
@@ -0,0 +1,13 @@
+package ch03.lecture.p02unary;
+
+public class C01Sign {
+    public static void main(String[] args) {
+        // -
+        int a = 3;
+        int b = -a;
+        System.out.println("b = " + b); // -3
+
+        int c = -b;
+        System.out.println("c = " + c); // 3
+    }
+}
