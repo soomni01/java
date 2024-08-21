0f1e834dc3506b18d759203fcf514ebbc5e58dae
soomni01 <dsgh8933@naver.com>
2024-08-21 14:55:43 +0900

    for

---
+++ src/ch04/lecture/p04for/C01For.java
@@ -0,0 +1,11 @@
+package ch04.lecture.p04for;
+
+public class C01For {
+    public static void main(String[] args) {
+        int i = 0;
+
+        for (i = 0; i < 3; i++) {
+            System.out.println("명령문");
+        }
+    }
+}
