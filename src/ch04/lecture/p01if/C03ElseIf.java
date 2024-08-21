f6f98869cab0558549d13357bcb03157c8127091
soomni01 <dsgh8933@naver.com>
2024-08-21 09:54:52 +0900

    else if 문

---
+++ src/ch04/lecture/p01if/C03ElseIf.java
@@ -0,0 +1,20 @@
+package ch04.lecture.p01if;
+
+public class C03ElseIf {
+    public static void main(String[] args) {
+        System.out.println(1);
+        if (false) {
+            System.out.println(2);
+            System.out.println(3);
+        } else if (false) {
+            System.out.println(4);
+            System.out.println(5);
+        } else if (true) {
+            System.out.println(6);
+            System.out.println(7);
+        } else {
+            System.out.println(8);
+            System.out.println(9);
+        }
+    }
+}
