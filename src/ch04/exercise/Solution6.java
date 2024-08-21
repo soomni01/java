54ce97382fd1a79a8af11d5a5a1b7567ee2a2cab
soomni01 <dsgh8933@naver.com>
2024-08-21 17:31:04 +0900

    확인문제 6번

---
+++ src/ch04/exercise/Solution6.java
@@ -0,0 +1,11 @@
+package ch04.exercise;
+
+public class Solution6 {
+    public static void main(String[] args) {
+        String star = "*";
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(star);
+            star += "*";
+        }
+    }
+}
