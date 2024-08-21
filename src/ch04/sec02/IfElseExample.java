8dbbe3d51d475aa068d3d8b2ffede738cd2f831d
soomni01 <dsgh8933@naver.com>
2024-08-21 09:55:02 +0900

    else if 문 예제

---
+++ src/ch04/sec02/IfElseExample.java
@@ -0,0 +1,19 @@
+package ch04.sec02;
+
+import java.util.Scanner;
+
+public class IfElseExample {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("점수>");
+        int score = scanner.nextInt();
+
+        if (score >= 90) {
+            System.out.println("점수가 90보다 큽니다.");
+            System.out.println("등급은 A입니다.");
+        } else {
+            System.out.println("점수가 90보다 작습니다.");
+            System.out.println("등급은 B입니다.");
+        }
+    }
+}
