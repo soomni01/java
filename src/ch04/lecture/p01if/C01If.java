65eb503f1283f91617dad9cbd5468bb83ecfedeb
soomni01 <dsgh8933@naver.com>
2024-08-21 09:32:06 +0900

    if 문

---
+++ src/ch04/lecture/p01if/C01If.java
@@ -0,0 +1,22 @@
+package ch04.lecture.p01if;
+
+public class C01If {
+    public static void main(String[] args) {
+        // if(조건문, 분긱문(branch))
+        System.out.println(1);
+
+        if (false) {
+            // 조건에 의해 실행되거나
+            // 실행되지 않도록 하고 싶은 코드들
+            System.out.println(2);
+            System.out.println(3);
+        }
+
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        System.out.println(4);
+        System.out.println(5);
+        System.out.println(6);
+    }
+}
