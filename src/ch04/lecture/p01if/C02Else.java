f94a30046806d7747789228b158a8166199c4d76
soomni01 <dsgh8933@naver.com>
2024-08-21 09:32:38 +0900

    if-else 문

---
+++ src/ch04/lecture/p01if/C02Else.java
@@ -0,0 +1,16 @@
+package ch04.lecture.p01if;
+
+public class C02Else {
+    public static void main(String[] args) {
+        System.out.println(1);
+        if (false) {
+            System.out.println(2);
+            System.out.println(3);
+        } else {
+            System.out.println(4);
+            System.out.println(5);
+        }
+        System.out.println(6);
+        System.out.println(7);
+    }
+}
