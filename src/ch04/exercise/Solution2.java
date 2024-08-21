672fe3dc1153176f4e94d68c552d9009fbc4b425
soomni01 <dsgh8933@naver.com>
2024-08-21 17:30:43 +0900

    확인문제 2번

---
+++ src/ch04/exercise/Solution2.java
@@ -0,0 +1,18 @@
+package ch04.exercise;
+
+public class Solution2 {
+    public static void main(String[] args) {
+        String grade = "B";
+
+
+        int score1 = switch (grade) {
+            case "A" -> 100;
+            case "B" -> {
+                int result = 100 - 20;
+                yield result;
+            }
+            default -> 60;
+        };
+        System.out.println(score1);
+    }
+}
