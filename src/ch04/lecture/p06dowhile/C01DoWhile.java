7ddae1e811120e5b088ea8c1e52be3e64d26164f
soomni01 <dsgh8933@naver.com>
2024-08-22 12:21:23 +0900

    do while

---
+++ src/ch04/lecture/p06dowhile/C01DoWhile.java
@@ -0,0 +1,11 @@
+package ch04.lecture.p06dowhile;
+
+public class C01DoWhile {
+    public static void main(String[] args) {
+        boolean a = false;
+
+        do {
+            System.out.println("명령문");
+        } while (a);
+    }
+}
