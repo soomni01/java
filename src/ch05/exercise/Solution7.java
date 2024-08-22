fc547cbe2f586fd82ccf4cab70c92dcde3cf0c85
soomni01 <dsgh8933@naver.com>
2024-08-22 16:50:04 +0900

    확인문제 7번

---
+++ src/ch05/exercise/Solution7.java
@@ -0,0 +1,15 @@
+package ch05.exercise;
+
+public class Solution7 {
+    public static void main(String[] args) {
+        int[] array = {1, 5, 3, 8, 2};
+        int max = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        System.out.println(max);
+    }
+}
