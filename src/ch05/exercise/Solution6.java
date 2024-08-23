2b0aa9b6c51b8aedb3040d181db62f8994e7a78e
soomni01 <dsgh8933@naver.com>
2024-08-23 12:34:56 +0900

    확인문제 6번

---
+++ src/ch05/exercise/Solution6.java
@@ -0,0 +1,14 @@
+package ch05.exercise;
+
+public class Solution6 {
+    public static void main(String[] args) {
+        int[][] array = {
+                {95, 86},
+                {83, 92, 96},
+                {78, 83, 93, 87, 88}
+        };
+
+        System.out.println(array.length);
+        System.out.println(array[2].length);
+    }
+}
