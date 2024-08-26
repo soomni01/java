6d959f108bad79b32324d951c57a3237625c5ce6
soomni01 <dsgh8933@naver.com>
2024-08-26 12:31:01 +0900

    substring 예제

--- src/ch05/sec05/SubStringExample.java
+++ src/ch05/sec05/SubStringExample.java
@@ -7,6 +7,7 @@ public class SubStringExample {
         String firstNum = ssn.substring(0, 5);
         System.out.println(firstNum);
 
+        // 7번째 문자부터 마지막 문자까지 추출
         String secondNum = ssn.substring(7);
         System.out.println(secondNum);
     }
