3e45eb796ce4388c84dfde6559124eb40485c254
soomni01 <dsgh8933@naver.com>
2024-08-27 08:55:41 +0900

    null

--- src/ch05/sec04/NullPointerExceptionExample.java
+++ src/ch05/sec04/NullPointerExceptionExample.java
@@ -1,6 +1,7 @@
 package ch05.sec04;
 
-public class NullPointerExceptionExample {
+public class
+NullPointerExceptionExample {
     public static void main(String[] args) {
         int[] intArray = null;
         // intArray[0] = 10; // NullPointerException
