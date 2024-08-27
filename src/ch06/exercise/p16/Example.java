52eeafd009c931da7eebae002769a7bf516119f3
soomni01 <dsgh8933@naver.com>
2024-08-27 12:31:46 +0900

    확인문제 16번

---
+++ src/ch06/exercise/p16/Example.java
@@ -0,0 +1,11 @@
+package ch06.exercise.p16;
+
+public class Example {
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+        printer.println(10);
+        printer.println(true);
+        printer.println(5.7);
+        printer.println("홍길동");
+    }
+}
---
+++ src/ch06/exercise/p16/Printer.java
@@ -0,0 +1,19 @@
+package ch06.exercise.p16;
+
+public class Printer {
+    public void println(int i) {
+        System.out.println(i);
+    }
+
+    public void println(boolean b) {
+        System.out.println(b);
+    }
+
+    public void println(double v) {
+        System.out.println(v);
+    }
+
+    public void println(String s) {
+        System.out.println(s);
+    }
+}
