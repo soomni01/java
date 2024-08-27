7462f4e5df7a535c627e351dd4841e94d0f2d64a
soomni01 <dsgh8933@naver.com>
2024-08-27 16:36:32 +0900

    생성자 오버로딩 예제

---
+++ src/ch06/sec07/sec04/Car.java
@@ -0,0 +1,26 @@
+package ch06.sec07.sec04;
+
+public class Car {
+    String company = "현대자동차";
+    String model;
+    String color;
+    int maxspeed;
+
+    Car() {
+    }
+
+    Car(String model) {
+        this.model = model;
+    }
+
+    Car(String model, String color) {
+        this.model = model;
+        this.color = color;
+    }
+
+    Car(String model, String color, int maxspeed) {
+        this.model = model;
+        this.color = color;
+        this.maxspeed = maxspeed;
+    }
+}
