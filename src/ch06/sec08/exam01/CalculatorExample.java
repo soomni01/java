a35a94b2ffffe7243318df9fd52dafabdfdab908
soomni01 <dsgh8933@naver.com>
2024-08-27 09:52:19 +0900

    method 예제

---
+++ src/ch06/sec08/exam01/CalculatorExample.java
@@ -0,0 +1,19 @@
+package ch06.sec08.exam01;
+
+public class CalculatorExample {
+    public static void main(String[] args) {
+        Calculator myCalc = new Calculator();
+
+        myCalc.powerON();
+
+        int result1 = myCalc.plus(5, 6);
+        System.out.println("result1 = " + result1);
+
+        int x = 10;
+        int y = 4;
+        double result2 = myCalc.divide(x, y);
+        System.out.println("result2 = " + result2);
+
+        myCalc.powerOff();
+    }
+}
