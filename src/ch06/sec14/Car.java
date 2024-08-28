58ee8f21abfc7eac82d7088483c86606389f6324
soomni01 <dsgh8933@naver.com>
2024-08-28 15:14:52 +0900

    encapsulation 예제

---
+++ src/ch06/sec14/Car.java
@@ -0,0 +1,27 @@
+package ch06.sec14;
+
+public class Car {
+    private int speed;
+    private boolean stop;
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        if (speed < 0) {
+            this.speed = 0;
+        } else {
+            this.speed = speed;
+        }
+    }
+
+    public boolean isStop() {
+        return stop;
+    }
+
+    public void setStop(boolean stop) {
+        this.stop = stop;
+        if (stop == true) this.speed = 0;
+    }
+}
