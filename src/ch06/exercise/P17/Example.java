1533401c40cf1dcbf4d2869ed2b144f2647cfe69
soomni01 <dsgh8933@naver.com>
2024-08-28 11:54:00 +0900

    확인문제 17번

---
+++ src/ch06/exercise/P17/Example.java
@@ -0,0 +1,10 @@
+package ch06.exercise.P17;
+
+public class Example {
+    public static void main(String[] args) {
+        Printer.println(10);
+        Printer.println(true);
+        Printer.println(5.7);
+        Printer.println("홍길동");
+    }
+}
---
+++ src/ch06/exercise/P17/Printer.java
@@ -0,0 +1,19 @@
+package ch06.exercise.P17;
+
+public class Printer {
+    static public void println(int i) {
+        System.out.println(i);
+    }
+
+    static public void println(boolean b) {
+        System.out.println(b);
+    }
+
+    static public void println(double v) {
+        System.out.println(v);
+    }
+
+    static public void println(String s) {
+        System.out.println(s);
+    }
+}
