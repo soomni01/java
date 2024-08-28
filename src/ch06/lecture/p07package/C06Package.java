02935168c82b1162e3fa4e79f194b889c6f09cde
soomni01 <dsgh8933@naver.com>
2024-08-28 12:44:02 +0900

    package

--- src/ch06/lecture/p07package/C06Package.java
+++ src/ch06/lecture/p07package/C06Package.java
@@ -1,18 +1,18 @@
 package ch06.lecture.p07package;
 
 //import java.io.FileInputStream;
-////import java.io.FileNotFoundException;
-////import java.io.FileOutputStream;
+//import java.io.FileNotFoundException;
+//import java.io.FileOutputStream;
 
-import java.io.*; // 같은 패키지의 여러 글래스 한 번에 import
+import java.io.*; // 같은 패키지의 여러 클래스 한 번에 import
 import java.util.Scanner;
 
 public class C06Package {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner scanner = new Scanner(System.in);
 
-        FileInputStream f = new FileInputStream();
-        FileOutputStream r = new FileOutputStream();
+//        FileInputStream f = new FileInputStream(a);
+//        FileOutputStream r = new FileOutputStream(args);
 
     }
 }
