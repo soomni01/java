faecfbd49844bdd89eb7d3d26aadb295f0068f3b
soomni01 <dsgh8933@naver.com>
2024-08-28 10:34:34 +0900

    static block 예제

--- src/ch06/sec10/exam01/Calculator.java
+++ src/ch06/sec10/exam01/Calculator.java
@@ -1,7 +1,7 @@
 package ch06.sec10.exam01;
 
 public class Calculator {
-    static double pi = 3.14159;
+    static double pi = 3.141592;
 
     static int plus(int x, int y) {
         return x + y;
