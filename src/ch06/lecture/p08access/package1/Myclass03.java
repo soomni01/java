68829d2f654c0e7c0b5b7ec5094057ec4b505847
soomni01 <dsgh8933@naver.com>
2024-08-28 12:43:05 +0900

    접근제한자

---
+++ src/ch06/lecture/p08access/package1/Myclass03.java
@@ -0,0 +1,12 @@
+package ch06.lecture.p08access.package1;
+
+public class Myclass03 {
+    // 어디서든지 접근 가능
+    public String name;
+
+    // 같은 패키지 내에서만 접근 가능
+    String email;
+
+    // 클래스 내에서만 접근 가능
+    private int age;
+}
