199a963d04872647dfba064ca053e680f3f34933
soomni01 <dsgh8933@naver.com>
2024-08-29 16:42:32 +0900

    다형성 예제

---
+++ src/ch07/sec07/exam02/ChildExample.java
@@ -0,0 +1,13 @@
+package ch07.sec07.exam02;
+
+public class ChildExample {
+    public static void main(String[] args) {
+        Child child = new Child();
+
+        Parent parent = child;
+
+        parent.method1();
+        parent.method2();
+//        parent.method3(); // 호출 불가능
+    }
+}
