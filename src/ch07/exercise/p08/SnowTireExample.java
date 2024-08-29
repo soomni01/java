498694c248d025e936502ed00c7b8236d5595e99
soomni01 <dsgh8933@naver.com>
2024-08-29 12:21:34 +0900

    확인문제 8번

---
+++ src/ch07/exercise/p08/SnowTireExample.java
@@ -0,0 +1,11 @@
+package ch07.exercise.p08;
+
+public class SnowTireExample {
+    public static void main(String[] args) {
+        SnowTire snowTire = new SnowTire();
+        Tire tire = new Tire();
+
+        snowTire.run();
+        tire.run();
+    }
+}
