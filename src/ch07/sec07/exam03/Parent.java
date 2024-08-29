6d86b2d0d7027200b105234eb5a7481ca21c1b4f
soomni01 <dsgh8933@naver.com>
2024-08-29 16:42:51 +0900

    강제 형변환 예제

---
+++ src/ch07/sec07/exam03/Parent.java
@@ -0,0 +1,13 @@
+package ch07.sec07.exam03;
+
+public class Parent {
+    public String field1;
+
+    public void method1() {
+        System.out.println("parent-method1()");
+    }
+
+    public void method2() {
+        System.out.println("parent-method2()");
+    }
+}
