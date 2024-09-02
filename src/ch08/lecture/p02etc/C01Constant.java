1b1a796ecbd130397f7b109dd14505cd0c2ac94c
soomni01 <dsgh8933@naver.com>
2024-09-02 11:29:11 +0900

    interface의 필드는 public static final

---
+++ src/ch08/lecture/p02etc/C01Constant.java
@@ -0,0 +1,16 @@
+package ch08.lecture.p02etc;
+
+public class C01Constant {
+}
+
+interface MyInterface1 {
+    // public final static 필드(상수, constant)
+    int AGE = 0;
+    static String NAME = "SON";
+    final int NUMBER = 999;
+    final static double PI = 3.141592;
+    public final static String MY_ADDRESS = "Korea";
+
+    // public abstract 메소드
+    void method1();
+}
