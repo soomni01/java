3253213dd97de1eaab4533b09bbad8531d26edd6
soomni01 <dsgh8933@naver.com>
2024-09-02 11:30:32 +0900

    인터페이스 간 상속

---
+++ src/ch08/sec09/InterfaceCImpl.java
@@ -0,0 +1,18 @@
+package ch08.sec09;
+
+public class InterfaceCImpl implements InterfaceC {
+    @Override
+    public void methodA() {
+        System.out.println("InterfaceCImpl.methodA");
+    }
+
+    @Override
+    public void methodB() {
+        System.out.println("InterfaceCImpl.methodB");
+    }
+
+    @Override
+    public void methodC() {
+        System.out.println("InterfaceCImpl.methodC");
+    }
+}
