bc252027b09ef2d7c6b064605fa76f28dcb4dc80
soomni01 <dsgh8933@naver.com>
2024-09-02 11:31:11 +0900

    private method

---
+++ src/ch08/sec07/Service.java
@@ -0,0 +1,33 @@
+package ch08.sec07;
+
+public interface Service {
+    default void defaultMethod1() {
+        System.out.println("defaultMethod1 종속 코드");
+        defaultCommon();
+    }
+
+    default void defaultMethod2() {
+        System.out.println("defaultMethod2 종속 코드");
+        defaultCommon();
+    }
+
+    private void defaultCommon() {
+        System.out.println("defaultMethod 중복 코드 A");
+        System.out.println("defaultMethod 중복 코드 B");
+    }
+
+    static void staticMethod1() {
+        System.out.println("StaticMethod1 종속 코드");
+        staticCommon();
+    }
+
+    static void staticMethod2() {
+        System.out.println("StaticMethod2 종속 코드");
+        staticCommon();
+    }
+
+    private static void staticCommon() {
+        System.out.println("staticMethod 중복 코드C");
+        System.out.println("staticMethod 중복 코드D");
+    }
+}
