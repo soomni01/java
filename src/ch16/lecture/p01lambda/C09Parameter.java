2e3ab52b7c0d6091ce5de73c081adc2962933aff
soomni01 <dsgh8933@naver.com>
2024-09-03 12:24:47 +0900

    파라미터

--- src/ch16/lecture/p01lambda/C09Parameter.java
+++ src/ch16/lecture/p01lambda/C09Parameter.java
@@ -11,7 +11,7 @@ public class C09Parameter {
         MyInterface9 p = (x) -> System.out.println(x * 2);
         p.method(7);
 
-        // 파라미터가 하나면 () 괄호 생략 가능(선호하는 방법
+        // 파라미터가 하나면 () 괄호 생략 가능(선호하는 방법)
         MyInterface9 q = (x) -> System.out.println(x * x);
         p.method(3);
 
