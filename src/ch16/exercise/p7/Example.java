38bdc8ab6c04a0e573fe305563ddae5f3afc9129
soomni01 <dsgh8933@naver.com>
2024-09-03 12:20:24 +0900

    확인문제 7번

---
+++ src/ch16/exercise/p7/Example.java
@@ -0,0 +1,36 @@
+package ch16.exercise.p7;
+
+public class Example {
+    private static int[] scores = {10, 50, 3};
+
+    public static int maxOrMin(Operator operator) {
+        int result = scores[0];
+        for (int score : scores) {
+            result = operator.apply(result, score);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        // 최대값 얻기
+        int max = maxOrMin((x, y) -> {
+            if (x > y) {
+                return x;
+            } else {
+                return y;
+            }
+        });
+        System.out.println("최대값: " + max);
+
+        // 최소값 얻기
+        int min = maxOrMin((x, y) -> {
+            if (x < y) {
+                return x;
+            } else {
+                return y;
+            }
+        });
+
+        System.out.println("최소값: " + min);
+    }
+}
