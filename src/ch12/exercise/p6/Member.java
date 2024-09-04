9bdbb59c8fea503f25f1c1ef04371bfca9b04b90
soomni01 <dsgh8933@naver.com>
2024-09-04 16:45:15 +0900

    확인문제 6번

---
+++ src/ch12/exercise/p6/Member.java
@@ -0,0 +1,16 @@
+package ch12.exercise.p6;
+
+public class Member {
+    private String id;
+    private String name;
+
+    public Member(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return id + ": " + name;
+    }
+}
