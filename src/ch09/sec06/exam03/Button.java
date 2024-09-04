302f2814a94f019a81862258cfed9fe5d19cc3af
soomni01 <dsgh8933@naver.com>
2024-09-04 11:52:52 +0900

    중첩 인터페이스

---
+++ src/ch09/sec06/exam03/Button.java
@@ -0,0 +1,17 @@
+package ch09.sec06.exam03;
+
+public class Button {
+    public static interface ClickListener {
+        void onClick();
+    }
+
+    private ClickListener clickListener;
+
+    public void setClickListener(ClickListener clickListener) {
+        this.clickListener = clickListener;
+    }
+
+    public void click() {
+        this.clickListener.onClick();
+    }
+}
