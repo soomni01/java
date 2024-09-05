a383ae27ad1e6f73339954475927609aa235c1e8
soomni01 <dsgh8933@naver.com>
2024-09-05 12:00:46 +0900

    비교

---
+++ src/ch12/lecture/p02wrapper/C05Wrapper.java
@@ -0,0 +1,12 @@
+package ch12.lecture.p02wrapper;
+
+public class C05Wrapper {
+    public static void main(String[] args) {
+        Integer a = 5000;
+        Integer b = 7000;
+        Integer c = 5000;
+
+        System.out.println(a <= b);
+        System.out.println(a <= c);
+    }
+}
