7fa76048c8103f67ebe6da5560b7a06f9b45b7ee
soomni01 <dsgh8933@naver.com>
2024-09-06 10:50:36 +0900

    확인문제 2번

---
+++ src/ch13/exercise/p2/Container.java
@@ -0,0 +1,13 @@
+package ch13.exercise.p2;
+
+class Container<T> {
+    public T item;
+
+    public T get() {
+        return item;
+    }
+
+    public void set(T item) {
+        this.item = item;
+    }
+}
--- src/ch13/exercise/p2/ContainerExample.java
+++ src/ch13/exercise/p2/ContainerExample.java
@@ -12,14 +12,3 @@ public class ContainerExample {
     }
 }
 
-class Container<T> {
-    public T item;
-
-    public T get() {
-        return item;
-    }
-
-    public void set(T item) {
-        this.item = item;
-    }
-}
