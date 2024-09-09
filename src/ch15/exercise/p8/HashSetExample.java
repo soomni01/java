c74bfa28962386f557f4a9f7eb302bfeb64b2bf3
soomni01 <dsgh8933@naver.com>
2024-09-09 16:50:56 +0900

    확인문제 8번

---
+++ src/ch15/exercise/p8/HashSetExample.java
@@ -0,0 +1,18 @@
+package ch15.exercise.p8;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetExample {
+    public static void main(String[] args) {
+        Set<Student> set = new HashSet<>();
+        set.add(new Student(1, "홍길동"));
+        set.add(new Student(2, "신용권"));
+        set.add(new Student(3, "조민우"));
+
+        System.out.println("저장된 객체 수: " + set.size());
+        for (Student s : set) {
+            System.out.println(s.studentNum + ":" + s.name);
+        }
+    }
+}
