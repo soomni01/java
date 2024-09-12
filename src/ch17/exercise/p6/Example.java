a9461d96a4c2bf26f4f971b4975026fe2be92cf6
soomni01 <dsgh8933@naver.com>
2024-09-12 17:14:38 +0900

    match

---
+++ src/ch17/exercise/p6/Example.java
@@ -0,0 +1,22 @@
+package ch17.exercise.p6;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Example {
+    public static void main(String[] args) {
+        List<Member> list = Arrays.asList(
+                new Member("홍길동", 30),
+                new Member("신용권", 40),
+                new Member("감자바", 26)
+        );
+
+        double avg = list.stream()
+                .collect(
+                        Collectors.averagingDouble(Member::getAge)
+                );
+
+        System.out.println("평균 나이: " + avg);
+    }
+}
