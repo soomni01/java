b91f4ebe893a0f1dfa2b85806378144d3850dafd
soomni01 <dsgh8933@naver.com>
2024-09-19 17:31:03 +0900

    read

---
+++ src/ch18/sec03/exam01/ReadExample.java
@@ -0,0 +1,23 @@
+package ch18.sec03.exam01;
+
+import java.io.*;
+
+public class ReadExample {
+    public static void main(String[] args) {
+        try {
+            InputStream is = new FileInputStream("C:/Temp/test1.db");
+
+            while (true) {
+                int data = is.read();
+                if (data == -1) break;
+                System.out.println(data);
+            }
+
+            is.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
