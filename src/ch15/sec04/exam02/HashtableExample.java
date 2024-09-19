856c41d93b4d45459250a08fbf893bbbf182197e
soomni01 <dsgh8933@naver.com>
2024-09-19 17:20:36 +0900

    io stream

---
+++ src/ch15/sec04/exam02/HashtableExample.java
@@ -0,0 +1,43 @@
+package ch15.sec04.exam02;
+
+
+import java.util.Hashtable;
+import java.util.Map;
+
+public class HashtableExample {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new Hashtable<>();
+
+        Thread threadA = new Thread() {
+            @Override
+            public void run() {
+                for (int i = 1; i <= 1000; i++) {
+                    map.put(String.valueOf(i), i);
+                }
+            }
+        };
+
+        Thread threadB = new Thread() {
+            @Override
+            public void run() {
+                for (int i = 1001; i <= 2000; i++) {
+                    map.put(String.valueOf(i), i);
+                }
+            }
+        };
+
+        threadA.start();
+        threadB.start();
+
+        try {
+            threadA.join();
+            threadB.join();
+        } catch (Exception e) {
+
+        }
+
+        int size = map.size();
+        System.out.println("총 엔트리 수: " + size);
+        System.out.println();
+    }
+}
