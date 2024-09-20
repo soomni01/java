4c5a8f9af9eee1c8e79402eb95fc8dd290669d8d
soomni01 <dsgh8933@naver.com>
2024-09-20 16:57:57 +0900

    flush

---
+++ src/ch18/lecture/p6buffer/C03Flush.java
@@ -0,0 +1,10 @@
+package ch18.lecture.p6buffer;
+
+public class C03Flush {
+    public static void main(String[] args) {
+        // flush
+        // : Buffer가 있는 OutputStream, Writer 에서
+        //   버퍼에 담긴 데이터를 목적지로 한 번에 전송
+
+    }
+}
